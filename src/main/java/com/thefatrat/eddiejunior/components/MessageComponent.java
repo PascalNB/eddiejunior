b290d952c1eb7541340f6765070c186ea589a694
pascal <p.n.bakker@student.utwente.nl>
2023-04-01 16:22:51 +0200

    better message edit

--- src/main/java/com/thefatrat/eddiejunior/components/MessageComponent.java
+++ src/main/java/com/thefatrat/eddiejunior/components/MessageComponent.java
@@ -1,19 +1,20 @@
 package com.thefatrat.eddiejunior.components;
 
 import com.thefatrat.eddiejunior.entities.Command;
+import com.thefatrat.eddiejunior.entities.Interaction;
 import com.thefatrat.eddiejunior.exceptions.BotErrorException;
+import com.thefatrat.eddiejunior.exceptions.BotWarningException;
 import com.thefatrat.eddiejunior.sources.Server;
 import com.thefatrat.eddiejunior.util.PermissionChecker;
 import com.thefatrat.eddiejunior.util.URLUtil;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
-import net.dv8tion.jda.api.utils.messages.MessageEditBuilder;
-import net.dv8tion.jda.api.utils.messages.MessageEditData;
-
-import java.util.regex.Pattern;
-import java.util.regex.PatternSyntaxException;
+import net.dv8tion.jda.api.interactions.components.text.TextInput;
+import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
+import net.dv8tion.jda.api.interactions.modals.Modal;
 
 public class MessageComponent extends Component {
 
@@ -25,17 +26,9 @@ public class MessageComponent extends Component {
         addSubcommands(
             new Command("edit", "edit a message sent by me")
                 .addOptions(
-                    new OptionData(OptionType.STRING, "message", "message jump url", true),
-                    new OptionData(OptionType.STRING, "regex", "regex", true),
-                    new OptionData(OptionType.STRING, "target", "target", true)
+                    new OptionData(OptionType.STRING, "message", "message jump url", true)
                 )
                 .setAction((command, reply) -> {
-                    String regex = command.getArgs().get("regex").getAsString();
-                    try {
-                        Pattern.compile(regex);
-                    } catch (PatternSyntaxException e) {
-                        throw new BotErrorException("Please input a valid regex");
-                    }
 
                     String url = command.getArgs().get("message").getAsString();
                     Message message = URLUtil.messageFromURL(url, getServer().getGuild());
@@ -47,38 +40,25 @@ public class MessageComponent extends Component {
                     PermissionChecker.requireSend(
                         message.getChannel().asGuildMessageChannel().getPermissionContainer());
 
-                    MessageEditBuilder builder = new MessageEditBuilder();
+                    String content = message.getContentRaw();
 
-                    try (MessageEditData data = MessageEditData.fromMessage(message)) {
-                        builder.applyData(data);
-                        String content = data.getContent();
-
-                        String target = command.getArgs().get("target").getAsString();
-                        target = target.replaceAll("\\\\n", "\n");
-                        String newContent = content.replaceAll(regex, target);
-
-                        if (newContent.isBlank()) {
-                            throw new BotErrorException("Cannot create a blank message");
-                        }
-
-                        builder.setContent(newContent);
-                    } catch (IllegalArgumentException e) {
-                        throw new BotErrorException("Couldn't use referenced message");
-                    } catch (IllegalStateException e) {
-                        throw new BotErrorException("Cannot reference an empty message");
+                    if (content.isEmpty()) {
+                        throw new BotErrorException("Message does not contain editable text");
                     }
 
-                    message.editMessage(builder.build())
-                        .onErrorMap(e -> null)
-                        .queue(m -> {
-                            if (m == null) {
-                                reply.hide();
-                                reply.send(new BotErrorException("Couldn't edit message"));
-                            } else {
-                                reply.ok("Message was edited");
-                                getServer().log(command.getMember().getUser(), "Edited message\n(%s)", url);
-                            }
-                        });
+                    TextInput input = TextInput.create(
+                            "message_" + message.getChannel().getId() + "_" + message.getId(),
+                            "Message", TextInputStyle.PARAGRAPH)
+                        .setRequired(true)
+                        .setValue(content)
+                        .setRequiredRange(1, 2000)
+                        .build();
+
+                    reply.sendModal(
+                        Modal.create("message_edit", "Edit message")
+                            .addActionRow(input)
+                            .build()
+                    );
                 }),
 
             new Command("remove", "remove a message sent by me")
@@ -97,6 +77,91 @@ public class MessageComponent extends Component {
                     });
                 })
         );
+
+        addMessageInteractions(
+            new Interaction<Message>("edit")
+                .setAction((event, reply) -> {
+                    Message message = event.getEntity();
+
+                    if (!message.getAuthor().getId().equals(getServer().getGuild().getSelfMember().getId())) {
+                        throw new BotErrorException("Message was not sent by me");
+                    }
+
+                    PermissionChecker.requireSend(
+                        message.getChannel().asGuildMessageChannel().getPermissionContainer());
+
+                    String content = message.getContentRaw();
+
+                    if (content.isEmpty()) {
+                        throw new BotErrorException("Message does not contain editable text");
+                    }
+
+                    TextInput input = TextInput.create(
+                            "message_" + message.getChannel().getId() + "_" + message.getId(),
+                            "Message", TextInputStyle.PARAGRAPH)
+                        .setRequired(true)
+                        .setValue(content)
+                        .setRequiredRange(1, 2000)
+                        .build();
+
+                    reply.sendModal(
+                        Modal.create("message_edit", "Edit message")
+                            .addActionRow(input)
+                            .build()
+                    );
+                }),
+
+            new Interaction<Message>("copy")
+                .setAction((event, reply) -> {
+                    Message message = event.getEntity();
+                    String content = message.getContentRaw();
+
+                    if (content.isEmpty()) {
+                        throw new BotWarningException("Message is empty");
+                    }
+
+                    content = content.replaceAll("```", "<codeblock>");
+
+                    if (content.length() > 2042) {
+                        content = content.substring(0, 2042);
+                    }
+
+                    reply.hide();
+                    reply.send("```%s```", content);
+                })
+        );
+
+        getServer().getModalHandler().addListener("message_edit", (event, reply) -> {
+            String key = event.getValues().keySet().iterator().next();
+            String[] split = key.split("_", 3);
+
+            TextChannel channel = getServer().getGuild().getTextChannelById(split[1]);
+            if (channel == null) {
+                throw new BotErrorException("Message could not be edited");
+            }
+
+            PermissionChecker.requireSend(channel.getPermissionContainer());
+
+            Message message = channel.retrieveMessageById(split[2])
+                .onErrorMap(e -> null)
+                .complete();
+
+            if (message == null) {
+                throw new BotErrorException("Message could not be edited");
+            }
+
+            String content = event.getValues().get(key).getAsString();
+            Message response = message.editMessage(content)
+                .onErrorMap(e -> null)
+                .complete();
+
+            if (response == null) {
+                throw new BotErrorException("Message could not be edited");
+            }
+            reply.hide();
+            reply.ok("Message has been edited");
+            getServer().log(event.getMember().getUser(), "Edited message\n(%s)", message.getJumpUrl());
+        });
 
     }
 
